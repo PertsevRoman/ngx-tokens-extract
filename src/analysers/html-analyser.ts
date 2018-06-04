a4bbb2ed81b2adf3688e85d6c7b10cd94a7730ea
Pertsev Roman <pertsev.roman@gmail.com>
2018-06-04 14:09:49 +0300

    [+] html parser complete

--- src/analysers/html-analyser.ts
+++ src/analysers/html-analyser.ts
@@ -1,6 +1,69 @@
+import * as parse5 from 'parse5';
+import * as fs from 'fs';
+
 import {getFilesList} from "./common";
 
 const delint = (filesList: string[]): string[] => {
+    let res: string[] = [];
+
+    const findDocumentBody = (document) => {
+        return document.childNodes[0].childNodes[1];
+    };
+
+    const clearTextValue = (text: string) : string[] => {
+        let res = [];
+
+        const regex = /\'([A-Z0-9\_]+\.)*[A-Z0-9\_]+\'\s*\|\s*translate/gm;
+
+        let regexResult: any;
+        let counter = 0;
+        while ((regexResult = regex.exec(text)) !== null) {
+            counter++;
+            let fullMatch = regexResult[0];
+            fullMatch = fullMatch.replace(`translate`, ``);
+            fullMatch = fullMatch.replace(/\}|\{|\||\'|\s*/g, ``);
+
+            res.push(fullMatch);
+        }
+
+        return res;
+    };
+
+    const walkNode = (node) => {
+        if (node.nodeName === `#text`) {
+            if (node.value.indexOf('translate') > -1) {
+                const message = clearTextValue(node.value);
+                res.push(...message);
+            }
+        } else {
+            if (node.attrs) {
+                node.attrs.forEach(({name, value}) => {
+                    if (value.indexOf('translate') > -1) {
+                        const message = clearTextValue(value);
+                        res.push(...message);
+                    }
+                });
+            }
+
+            if (node.childNodes) {
+                node.childNodes.forEach(walkNode);
+            }
+        }
+    };
+
+    for (let filePath of filesList) {
+        const fileContent = fs.readFileSync(filePath).toString();
+        const document = parse5.parse(fileContent);
+
+        try {
+            const body = findDocumentBody(document);
+
+            walkNode(body);
+        } catch (e) {
+        }
+    }
+
+    return res;
 };
 
 export const htmlFilesParse = (files: string | string[]): string[] => {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,88 @@
 import * as walk from 'walk';
 import {tsFilesParse} from "./analysers/ts-analyser";
+import {htmlFilesParse} from "./analysers/html-analyser";
+import * as fs from 'fs';
 
 const srcPath = `D:\\Users\\RAPertsev\\IdeaProjects\\vendor-ui\\src`;
+const i18Path = `D:\\Users\\RAPertsev\\IdeaProjects\\vendor-ui\\src\\assets\\i18n`;
+
+interface FileDescriptor {
+    name: string;
+    root?: string;
+    content?: string;
+}
+
+
 (() => {
-    const walker = walk.walk(srcPath);
+    const srcWalker = walk.walk(srcPath);
+
+    const replacer = (key, value) =>
+        value instanceof Object && !(value instanceof Array) ?
+            Object.keys(value)
+                .sort()
+                .reduce((sorted, key) => {
+                    sorted[key] = value[key];
+                    return sorted
+                }, {}) :
+            value;
 
     let tsFiles: string[] = [];
     let htmlFiles: string[] = [];
 
-    walker.on('file', (root, fileStats, next) => {
+    /**
+     *
+     * @param shadowTree
+     * @param sourceTree
+     * @param fileName
+     */
+    let stack = [];
+    const fillShadowBranches = (shadowTree: any, sourceTree: any, fileName: string) => {
+        for(let shadowKey in shadowTree) {
+            stack.push(shadowKey);
+
+            if(sourceTree[shadowKey]) {
+                if(typeof sourceTree[shadowKey] === 'object') {
+                    fillShadowBranches(shadowTree[shadowKey], sourceTree[shadowKey], fileName);
+                } else if(typeof sourceTree[shadowKey] === 'string') {
+                    shadowTree[shadowKey] = sourceTree[shadowKey];
+                }
+            } else {
+                if(!shadowTree[shadowKey]) {
+                    console.log(`WARNING: ${fileName}: translate path '${
+                        stack.join('.')}' is empty, please give value to that`);
+                }
+            }
+
+            stack.pop();
+        }
+    };
+
+    /**
+     *
+     * @param {string[][]} branches
+     * @return {{}}
+     */
+    const makeTranslatorTree = (branches: string[][]) => {
+        let shadowTranslateTree = {};
+        branches.forEach(branch => {
+            let mirror = shadowTranslateTree;
+            while (branch.length > 1) {
+                let key = branch.shift();
+
+                if(!mirror[key]) {
+                    mirror[key] = {};
+                }
+
+                mirror = mirror[key];
+            }
+
+            mirror[branch[0]] = '';
+        });
+
+        return shadowTranslateTree;
+    };
+
+    srcWalker.on('file', (root, fileStats, next) => {
         if (fileStats.name.endsWith(`.ts`) && !fileStats.name.endsWith(`.d.ts`)) {
             const fileFullPath = `${root}\\${fileStats.name}`;
             tsFiles.push(fileFullPath);
@@ -22,10 +96,47 @@ const srcPath = `D:\\Users\\RAPertsev\\IdeaProjects\\vendor-ui\\src`;
         next();
     });
 
+    srcWalker.on('end', () => {
+        const entries = [
+            ...tsFilesParse(tsFiles),
+            ...htmlFilesParse(htmlFiles),
+        ];
 
-    walker.on('end', () => {
-        const tokens: string[] = tsFilesParse(tsFiles);
+        let branches: string[][] = entries.map(token => token.split(`.`));
 
-        console.log(tokens);
+        let shadowTranslateTree: any = makeTranslatorTree(branches);
+
+        const i18Walker = walk.walk(i18Path);
+        const i18Files: FileDescriptor[] = [];
+        i18Walker.on(`file`, (root, fileStats, next) => {
+            if (fileStats.name.endsWith('.json') &&
+                !fileStats.name.endsWith('-static.json')) {
+                const fileFullPath = `${root}\\${fileStats.name}`;
+                const content = JSON.parse(fs.readFileSync(fileFullPath, {
+                    encoding: 'utf8'
+                }).toString());
+
+                i18Files.push({
+                    root,
+                    name: fileStats.name,
+                    content
+                });
+            }
+
+            next();
+        });
+
+        i18Walker.on(`end`, () => {
+            i18Files.forEach((i18File: FileDescriptor) => {
+                const shadowTreeCopy = Object.assign({}, shadowTranslateTree);
+
+                fillShadowBranches(shadowTreeCopy, i18File.content, i18File.name);
+
+                const translatePath = `${i18File.root}/${i18File.name}`;
+                const dumpContent = JSON.stringify(shadowTreeCopy, replacer, 2);
+
+                fs.writeFileSync(translatePath, dumpContent, 'utf8');
+            });
+        });
     });
 })();
